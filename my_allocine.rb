ef7149c1d1581de0f49198810e73d4352644c742
Murodjon Abdujabbor <107322722+murodjonabdujabbor33@users.noreply.github.com>
2022-08-09 18:07:41 +0500

    Update my_allocine.rb
    Addendum added.

--- my_allocine.rb
+++ my_allocine.rb
@@ -5,3 +5,7 @@ db = SQLite3::Database.open "movies.db"
 requests['Display all actors'] = "SELECT * FROM actors;"
 
 requests['Display all genres'] = "SELECT * FROM genres;"
+
+requests['Display the name and year of the movies'] = "SELECT mov_title, mov_year FROM movies;"
+
+requests['Display reviewer name from the reviews table'] = "SELECT rev_name FROM reviews;"
